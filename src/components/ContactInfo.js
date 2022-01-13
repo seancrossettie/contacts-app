2ad33182ef8f083e79e290864d8eeeee62490159
seancrossettie <seancrossettie@gmail.com>
2022-01-13 14:09:52 -0600

    fixed validation

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,17 +1,28 @@
-import { Button, ButtonGroup, Flex, Text } from '@chakra-ui/react';
+import { Button, ButtonGroup, Flex, Text, useDisclosure } from "@chakra-ui/react";
 import React from 'react';
+import { ContactModal } from "./ContactModal";
 
-export const ContactInfo = ({ contact, setInfo }) => {
+export const ContactInfo = ({ info, setInfo, contacts, setContacts }) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     return (
         <Flex flexDir={"column"}>
-            <Text fontSize={"4xl"} marginBottom={"1rem"}>{contact.name}</Text>
-            <Text fontSize={"lg"} marginBottom={"1rem"}>phone: {contact.phone}</Text>
-            <Text fontSize={"lg"} marginBottom={"1rem"}>email: {contact.email}</Text>
-            <Text fontSize={"lg"} marginBottom={"1rem"}>notes: {contact.notes}</Text>
+            <Text fontSize={"4xl"} marginBottom={"1rem"}>{info.name}</Text>
+            <Text fontSize={"lg"} marginBottom={"1rem"}>Phone: {info.phone}</Text>
+            <Text fontSize={"lg"} marginBottom={"1rem"}>Email: {info.email}</Text>
+            <Text fontSize={"lg"} marginBottom={"1rem"}>Notes: {info.notes}</Text>
             <ButtonGroup isAttached>
-                <Button>Update</Button>
+                <Button onClick={onOpen}>Update</Button>
                 <Button onClick={() => setInfo(null)}>Close</Button>
             </ButtonGroup>
+            <ContactModal 
+                isOpen={isOpen}
+                onClose={onClose}
+                info={info}
+                setInfo={setInfo}
+                contacts={contacts}
+                setContacts={setContacts}
+            />
         </Flex>
     );
 };
