8ee4515e2dbc37cc5521aa6dffcd8b6917e8ef31
seancrossettie <seancrossettie@gmail.com>
2022-01-13 15:00:02 -0600

    added validation to email

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# Contacts App
-Bootstrapped with ```npx create-react-app``` and with the following dependencies:
+# Contacts App [![Netlify Status](https://api.netlify.com/api/v1/badges/1a1ed47f-720f-427e-b8ea-877e5ba9ed68/deploy-status)](https://app.netlify.com/sites/quizzical-bohr-3f915f/deploys)
+
+## Deployed Site
+https://foundry-376-contacts.netlify.app/
+
+Bootstrapped with ```npx create-react-app``` and includes the following dependencies:
 - Chakra UI
 - Axios
+- validator.js
--- src/components/ContactModal.js
+++ src/components/ContactModal.js
@@ -1,5 +1,6 @@
 import { Button, ButtonGroup, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react';
-import React from 'react'
+import React, { useState } from 'react';
+import validator from 'validator';
 import { ContactForm } from './ContactForm';
 
 export const ContactModal = ({ isOpen, onClose, info, setInfo, contacts, setContacts }) => {
@@ -9,6 +10,17 @@ export const ContactModal = ({ isOpen, onClose, info, setInfo, contacts, setCont
         newContactArr[contacts.findIndex(i => i.id === info.id)] = info;
         setContacts(newContactArr);
         onClose();
+    };
+
+    
+    const [emailError, setEmailError] = useState(false);
+    const validateEmail = (e) => {
+        const email = e.target.value;
+        if (validator.isEmail(email)) {
+            setEmailError(false);
+        } else {
+            setEmailError(true);
+        }
     };
 
     return (
@@ -22,11 +34,13 @@ export const ContactModal = ({ isOpen, onClose, info, setInfo, contacts, setCont
                         <ContactForm 
                             info={info}
                             setInfo={setInfo}
+                            emailError={emailError}
+                            validateEmail={validateEmail}
                         />
                     </ModalBody>
                     <ModalFooter>
                         <ButtonGroup isAttached>
-                            <Button onClick={updateContact}>Update</Button>
+                            <Button isDisabled={emailError} onClick={updateContact}>Update</Button>
                             <Button onClick={onClose}>Close</Button>
                         </ButtonGroup>
                     </ModalFooter>
